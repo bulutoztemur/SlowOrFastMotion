88483e0964399182fbeb51ff605e3eaeaf870dc9
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2021-06-03 22:37:10 +0300

    Initial Commit

---
+++ SlowOrFastMotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlowOrFastMotion
+//
+//  Created by alaattinbulut on 3.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
